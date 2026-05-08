22fc35ef0ee22b7e796b27aa1d0b5259a1b9cbe2
Grizzed <k.v.malay@gmail.com>
2026-05-09 02:53:46 +0530

    Delete src/routes/_layout.tsx

--- src/routes/_layout.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/_layout")({
-  component: LayoutComponent,
-});
-
-function LayoutComponent() {
-  return <div>Layout</div>;
-}
